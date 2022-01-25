424415c1a9cdcc7fa2202eb03576e3c1d4f88245
moonmia <92148193+moonmia@users.noreply.github.com>
2022-01-25 13:43:24 -0800

    Time: 4 ms (98.78%), Space: 9 MB (19.73%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
-    stack<int> num;
-    stack<char> op; 
-
-    void eval()
-    {
-        int b = num.top(); num.pop();
-        int a = num.top(); num.pop();
-        char c= op.top(); op.pop();
-
-        int result;
-        if(c=='+') result=a+b;
-        else if(c=='-') result=a-b;
-        else if(c=='*') result=a*b;
-        else result = a/b;
-        num.push(result);
-    }
     int calculate(string s) 
     {
-        s = '0' + s; 
-        unordered_map<char, int>pr;
-        pr['+'] = pr['-'] =1, pr['*'] = pr['/'] =2; 
+        vector<int> stk;
+        char preSign = '+';
+        int num =0;
         for(int i=0; i<s.size(); i++)
         {
-            char c = s[i];
-            if(c == ' ') continue;
-            if(isdigit(c))
+            if(isdigit(s[i]))
             {
-                int sum =0;
-                while(i<s.size() && isdigit(s[i])) sum=sum*10 + (s[i++] - '0');
-                i--;
-                num.push(sum);
-                
+                num = num*10 + int (s[i]-'0');
             }
-            else
+            if(!isdigit(s[i]) && s[i] != ' ' || i == s.size()-1)
             {
-                while(op.size() && pr[op.top()] >= pr[c]) eval();
-                op.push(c);
+                switch(preSign)
+                {
+                    case '+':
+                        stk.push_back(num);
+                        break;
+                    case '-':
+                        stk.push_back(-num);
+                        break;
+                    case '*':
+                        stk.back() *= num;
+                        break;
+                    case '/':
+                        stk.back() /= num;
+                        break;
+                }
+                preSign =s[i];
+                num =0;
             }
         }
-        while(op.size()) eval();
-        return num.top();
+        return accumulate(stk.begin(), stk.end(), 0);
     }
 };
