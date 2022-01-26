1a053c4c5965a435e22ef03dfdb2b6b9d9268975
moonmia <92148193+moonmia@users.noreply.github.com>
2022-01-25 16:39:26 -0800

    Time: 34 ms (42.72%), Space: 18.9 MB (42.27%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
+    {
+        vector<vector<int>>result;
+        
+        if(intervals.size() == 0) return result;
+        
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {return a[0] <b[0];});
+        
+        result.push_back(intervals[0]);
+        
+        for(int i=1; i<intervals.size(); i++)
+        {
+            if(result.back()[1] >= intervals[i][0])
+            {
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
+            }
+            else
+            {
+                result.push_back(intervals[i]);
+            }
+        }
+        return result;
+    }
+};
