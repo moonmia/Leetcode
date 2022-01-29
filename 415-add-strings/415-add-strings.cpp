467355ae4ae9baa4f70ef3db54de256ebe8ab83a
moonmia <92148193+moonmia@users.noreply.github.com>
2022-01-28 17:51:01 -0800

    Time: 3 ms (83.07%), Space: 6.8 MB (68.96%) - LeetHub

--- 415-add-strings/415-add-strings.cpp
+++ 415-add-strings/415-add-strings.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     string addStrings(string num1, string num2) 
     {
-        if(num2.size()==0) return num1;
+        if(num2.size() == 0) return num1;
+        
+        int m =num1.size()-1, n = num2.size()-1, add=0;
         
         string result = "";
-        int n=num1.size()-1, m=num2.size()-1, add=0;
         
-        while(m>=0 || n>=0 || add != 0)
+        while(m>=0 || n >=0 || add != 0)
         {
-            int x=n>=0 ? num1[n]-'0' :0;
-            int y=m>=0 ? num2[m]-'0' :0;
+            int x = m >=0 ? num1[m] - '0' : 0;
+            int y = n >=0 ? num2[n] - '0' : 0;
+            
             int sum = x+y+add;
-            result.push_back('0' + sum %10);
+            result.push_back('0' + sum%10);
             add = sum/10;
             m--;
             n--;
-            
         }
         reverse(result.begin(), result.end());
         return result;
