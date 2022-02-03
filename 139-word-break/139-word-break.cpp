909c0fa59fd7f5b6e96ba8f8a39ac89e8dacc8ab
moonmia <92148193+moonmia@users.noreply.github.com>
2022-02-03 02:18:10 -0800

    Time: 35 ms (20.94%), Space: 14.7 MB (18.56%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -5,14 +5,12 @@ public:
         unordered_set<string>wordSet(wordDict.begin(), wordDict.end());
         vector<bool>dp(s.size()+1, false);
         dp[0] = true;
-        
-        for(int i=0; i<=s.size(); i++)
+        for(int i=1; i<=s.size(); i++)
         {
             for(int j=0; j<i; j++)
             {
                 string word = s.substr(j, i-j);
-                
-                if(wordSet.find(word) != wordSet.end() && dp[j])
+                if(wordSet.find(word) != wordSet.end() && dp[j] == true)
                 {
                     dp[i] = true;
                 }
