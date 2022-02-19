10e7d0255b2590ad002d922dab9b73e6cb9ff8f3
moonmia <92148193+moonmia@users.noreply.github.com>
2022-02-18 18:36:57 -0800

    Time: 405 ms (8.90%), Space: 9.3 MB (97.30%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) 
+    {
+        stack<int>stk;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(i == 0 || s[i] != s[i-1])
+            {
+                stk.push(1);
+            }
+            else if(++stk.top() == k)
+            {
+                stk.pop();
+                s.erase(i-k+1, k);
+                i = i-k;
+            }
+        }
+        return s;
+    }
+};
+
+//O(n) && O(n)
