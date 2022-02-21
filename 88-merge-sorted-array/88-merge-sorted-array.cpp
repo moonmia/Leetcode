a2d561822afddf5764e496b016d83214e85367fa
moonmia <92148193+moonmia@users.noreply.github.com>
2022-02-20 18:32:14 -0800

    Time: 3 ms (69.27%), Space: 8.9 MB (99.38%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
     {
-        int size = m+n-1;
+        int i = nums1.size()-1;
         m--;
         n--;
         
-        while(n>=0)
+        while(n >=0)
         {
-            while(m>=0 && nums1[m] > nums2[n])
+            while(m >=0 && nums1[m] > nums2[n])
             {
-                swap(nums1[m--], nums1[size--]);
+                swap(nums1[i--], nums1[m--]);
+                
             }
-            swap(nums1[size--], nums2[n--]);
+            swap(nums1[i--], nums2[n--]);
         }
+        
     }
 };
