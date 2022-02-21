f8ef91dcb1eba653d128a7f0b5cf28278210024a
moonmia <92148193+moonmia@users.noreply.github.com>
2022-02-20 16:33:33 -0800

    Time: 5 ms (36.54%), Space: 5.9 MB (93.65%) - LeetHub

--- 65-valid-number/65-valid-number.cpp
+++ 65-valid-number/65-valid-number.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool hasE, hasNum, isFloat;
-    bool isNumber(string& s) 
+    bool hasNum, hasE, hasDot;
+    bool isNumber(string s) 
     {
         for(int i=0; i<s.size(); i++)
         {
-            auto c=s[i];
-            if((c == '-' || c=='+') && (i==0 || s[i-1] == 'e' || s[i-1] == 'E'))
+            auto c = s[i];
+            if((c == '+' || c == '-') && (i==0 || s[i-1] == 'e' || s[i-1] == 'E'))
             {
-                
+                continue;
             }
-            else if((c=='e' || c=='E') && !hasE && hasNum)
+            else if(c == '.' && !hasDot && !hasE)
             {
-                hasE=true;
+                hasDot = true;
+            }
+            else if((c == 'e' || c == 'E') && !hasE && hasNum)
+            {
+                hasE = true;
                 hasNum = false;
-            }
-            else if(c=='.' && !isFloat && !hasE)
-            {
-                isFloat = true;
             }
             else if(isdigit(c))
             {
-                hasNum=true;
+                hasNum = true;
             }
             else
             {
