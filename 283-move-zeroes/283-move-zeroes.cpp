d51de3b027f09be3792b93b9eb4e69c63ddc8684
moonmia <92148193+moonmia@users.noreply.github.com>
2022-02-21 22:31:40 -0800

    Time: 31 ms (58.01%), Space: 19 MB (98.30%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int n= nums.size(), left=0, right=0;
+        
+        while(right <n)
+        {
+            if(nums[right])
+            {
+                swap(nums[right], nums[left]);
+                left++;
+            }
+            right ++;
+        }
+    }
+};
