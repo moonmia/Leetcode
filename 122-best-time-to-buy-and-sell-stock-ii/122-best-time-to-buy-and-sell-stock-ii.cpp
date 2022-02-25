8fad4ea4244585e87b9627d718367c6582be5650
moonmia <92148193+moonmia@users.noreply.github.com>
2022-02-25 00:11:31 -0800

    Time: 3 ms (96.19%), Space: 13 MB (87.14%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -5,7 +5,7 @@ public:
         int sum =0;
         for(int i=1; i<prices.size(); i++)
         {
-            sum += max(0, prices[i]- prices[i-1]);
+            sum += max(0, prices[i]-prices[i-1]);
         }
         return sum;
     }
