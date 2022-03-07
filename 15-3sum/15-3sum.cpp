8ed66201286f6d2d7a3e0fe6106eae0224fd6697
moonmia <92148193+moonmia@users.noreply.github.com>
2022-03-06 19:30:21 -0800

    Time: 110 ms (65.84%), Space: 20 MB (87.06%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
-        int n= nums.size();
         vector<vector<int>>result;
-        
-        
         sort(nums.begin(), nums.end());
         
-        for(int i=0; i<n; i++)
+        for(int i=0; i<nums.size(); i++)
         {
-            if(nums[i] > 0) return result;
+            if(nums[i] >0) return result;
             if(i>0 && nums[i] == nums[i-1]) continue;
-            int left = i+1, right = n-1;
+            int left =i+1, right=nums.size()-1;
+            
             
             while(right > left)
             {
-                if(nums[i] + nums[left] + nums[right] > 0) right --;
-                else if(nums[i] + nums[left] + nums[right] <0) left++;
-                else 
+                
+                
+                if(nums[i]+nums[left]+nums[right] <0) left++;
+                else if(nums[i]+nums[left]+nums[right] >0) right--;
+                else
                 {
                     result.push_back(vector<int>{nums[i], nums[left], nums[right]});
-                    while(right > left && nums[right] ==  nums[right-1]) right--;
-                    while(right > left && nums[left] == nums[left+1]) left++;
-                    
-                    right--;
+                    while(right>left && nums[right] == nums[right-1]) right--;
+                    while(right>left && nums[left] == nums[left+1]) left++;
                     left++;
+                    right--;
                 }
             }
+            
         }
         return result;
     }
