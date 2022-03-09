4b42bd2366aa250fa00fbb34721dd710ba6d3823
moonmia <92148193+moonmia@users.noreply.github.com>
2022-03-09 15:19:26 -0800

    Time: 0 ms (100.00%), Space: 7.8 MB (24.92%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int rob(vector<int>& nums) 
     {
-        if(nums.empty())
-        {
-            return 0;
-        }
-        int size = nums.size();
-        if(size ==1) return nums[0];
-        vector<int> dp = vector<int>(size, 0);
+        if(nums.size()==1) return nums[0];
+        vector<int>dp(nums.size()+1, 0);
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
-        
-        for(int i=2; i<size; i++)
+        for(int i=2; i<nums.size(); i++)
         {
-            dp[i] = max(dp[i-2]+nums[i], dp[i-1]);
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
         }
-        return dp[size-1];
+        return dp[nums.size()-1];
     }
 };
+/*
+dp[0] = nums[0];
+dp[1] = max(nums[0], nums[1]);
+dp[2] = max(dp[1], nums[1]) = max(1, 2) = 2
+dp[3] = max(dp[1]+nums[2], dp[2]) = max(1+3,2) =4
+dp[4] = max(dp[3], dp[2]+nums[3]) = (4, 3) = 4
+dp[i] = max(dp[i-1], dp[i-2]+nums[i-1])
+*/
